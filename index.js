8ca8d495f145ac4cfb0fdaa82278e1a0b8000a9c
Andrew <a.oppman@gmail.com>
2023-07-23 01:07:55 -0400

    creation of JS variables for HTML elements 'lists' in index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const el = (id) => document.getElementById(id);
+
+const pancakes = [el('pancake-ingredients'), el('pancake-instructions')];
+const eggs = [el('egg-ingredients'), el('egg-instructions')];
+const chickn = [el("chick\'n-ingredients"), el("chick\'n-instructions")];   
+const burger = [el("burger-ingredients"), el("burger-instructions")];
+const taco = [el("taco-ingredients"), el("taco-instructions")];
+const phork = [el("phork-ingredients"), el("phork-instructions")];
