c89712e613bf8fded41bd4a14b5e7bca6e0cba70
Andrew <a.oppman@gmail.com>
2023-07-24 18:18:53 -0400

    created function to append specific ol/ul to each card

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const el = (id) => document.getElementById(id);
 const API = "http://localhost:3000/veganMeals";
 
+//Meal ingredients and instructions
 const pancakes = [el('pancake-ingredients'), el('pancake-instructions')];
 const eggs = [el('egg-ingredients'), el('egg-instructions')];
 const chickn = [el("chick\'n-ingredients"), el("chick\'n-instructions")];   
@@ -12,6 +13,7 @@ const headerImg1 = el("try-vegan");
 const headerImg2 = el("mid-east-vegan");
 const submitBtn = el("submit");
 
+// Form functionality
 function selectMeal() {
     const selectedMeal = el("meal").value;
     switch (selectedMeal) {
@@ -33,9 +35,8 @@ function selectMeal() {
 
 submitBtn.addEventListener('click', selectMeal);
 
-function submit() {
-    submitBtn.addEventListener('submit', )
-}
+
+
 
 
 fetch(API)
@@ -48,7 +49,7 @@ console.log(mealList);
 
 const mealCards = mealList.forEach((meal) => {
     const card = document.createElement('div');
-    card.classList.add("card", `${meal.class}`);
+    card.classList.add(`${meal.class}`);
 
 
     const h2 = document.createElement('h2');
@@ -64,9 +65,33 @@ const mealCards = mealList.forEach((meal) => {
     p.textContent = meal.srcLink;
     card.append(h2, img, p);
 
-    console.log(card)
+    document.querySelector('#card-holder').append(card);
+
+//append ul / ol to cards
+    function appendInfo(meal){
+        switch (meal.id) {
+         case 1:
+             return card.append(pancakes[0], pancakes[1]);
+         break;
+         case 2:
+             return card.append(eggs[0], eggs[1]);
+         break;
+         case 3:
+             return card.append(chickn[0], chickn[1]);
+         break;
+         case 4:
+             return card.append(burger[0], burger[1]);
+         break;
+         case 5:
+             return card.append(taco[0], taco[1]);
+         break;
+         case 6:
+             return card.append(phork[0], phork[1]);
+         break;
+         default: return false
+      }
+     };
+    appendInfo(meal);
 });
 }
 
-const cardContainer = document.createElement('div');
-cardContainer.classList.add('container');
