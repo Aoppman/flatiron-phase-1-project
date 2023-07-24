dee679a0ab3720e14f5bdc11e862e345c77ac50c
Andrew <a.oppman@gmail.com>
2023-07-23 22:35:53 -0400

    event listener/function added for meal category select

--- index.js
+++ index.js
@@ -6,6 +6,42 @@ const chickn = [el("chick\'n-ingredients"), el("chick\'n-instructions")];
 const burger = [el("burger-ingredients"), el("burger-instructions")];
 const taco = [el("taco-ingredients"), el("taco-instructions")];
 const phork = [el("phork-ingredients"), el("phork-instructions")];
-
+const selector = el("meal");
 const headerImg1 = el("try-vegan");
 const headerImg2 = el("mid-east-vegan");
+const submitBtn = el("submit");
+
+function selectMeal() {
+    const selectedMeal = el("meal").value;
+    switch (selectedMeal) {
+        case "breakfast":
+            alert("You've selected Breakfast! Let's see what's cooking 👨‍🍳");
+            break;
+        case "lunch":
+            alert("You've selected Lunch! Let's see what's cooking 👨‍🍳");
+            break;
+        case "dinner":
+            alert("You've selected Dinner! Let's see what's cooking 👨‍🍳");
+            break;
+        default:
+            alert("Please select a meal category!");
+            return false;
+    }
+}
+
+submitBtn.addEventListener('click', selectMeal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const card = document.createElement('div');
